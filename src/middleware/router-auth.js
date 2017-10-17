4a6b8e81ee71de8e51bc92a1cd053752d8508813
GavinThomas1192 <gthomas1192@gmail.com>
2017-10-17 15:04:05 -0700

    newnew

--- src/middleware/router-auth.js
+++ src/middleware/router-auth.js
@@ -38,11 +38,11 @@ export default new Router()
         console.log('my oauth token:', token);
         res.cookie('X-Slugchat-Token', token);
         // console.log(res)
-        res.redirect(process.env.CLIENT_URL);
+        res.redirect(process.env.FRONT_URL);
       })
       .catch((error) => {
         console.error(error);
-        res.redirect(process.env.CLIENT_URL);
+        res.redirect(process.env.FRONT_URL);
       })
   }
 })
